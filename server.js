374f22a73e3e84e841f530163611d7ab0bf29c4c
elliotmoose <elliot_koh_1997@yahoo.com.sg>
2018-09-10 14:14:04 +0800

    change port to 8080

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ app.get('/', function(req, res){
   res.sendFile(__dirname+'/index.html');
 });
 
-http.listen(80, function(){
-  console.log('listening on 80');
+http.listen(8080, function(){
+  console.log('listening on 8080');
 });
 
 
